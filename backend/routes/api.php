3c63b9da6a2a9738e1425af7a2ec169e5597d0e5
BISNISPRJCK <bisnisprojeck025@gmail.com>
2024-10-15 12:52:36 +0700

    menerima semua konflik commit mila

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AlatController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SejarahController;
 use App\Models\ProfilePerusahaan;
+
 use Illuminate\Support\Facades\Route;
 
 // Route untuk mendapatkan semua data alat
@@ -13,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/alat', [AlatController::class, 'index']);
 Route::post('/addAlat', [AlatController::class, 'store']);
 
-
+// Profile perusahaan
 Route::post('/storeProfile', [ProfileController::class, 'store']);
 Route::get('/Profile', [ProfileController::class, 'index']);
 Route::get('/Profile/{id}', [ProfileController::class, 'show']);
+
+// sejarah 
+Route::post('/storeSejarah', [SejarahController::class, 'storeSejarah']);
+Route::get('/showSejarah/{id}', [SejarahController::class, 'sejarahShow']);
