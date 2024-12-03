3f5cf9ee90d477838c7beb670a3bd28b662c9a8d
Ririn333 <ririnnurlela425@gmail.com>
2024-12-03 14:18:38 +0700

    meresponsifkan kegiatan, sejarah, dan navbar

--- frontend/src/component/kegiatan.jsx
+++ frontend/src/component/kegiatan.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import Navigator from "./Navbar";
 import BackToTop from "./BackToTop";
 
 const Kegiatan = () => {
   const [kegiatanData, setKegiatanData] = useState([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     axios
@@ -15,125 +20,27 @@ const Kegiatan = () => {
       .catch((error) => {
         console.log("Terjadi Error Saat Mengambil data kegiatan:", error);
       });
-  });
+  }, []);
 
   return (
     <>
+    <Navigator/>
       <BackToTop />
-      <header>
-        <nav className="bg-blue-900 shadow-md fixed w-800 top-[3%] left-3 right-3 z-10 flex justify-between items-center p-4 rounded-full h-18">
-          <div className="flex items-center">
-            <img src="logo.png" alt="Logo" className="h-12 mr-2" />
-            <div className="text-white text-2xl font-bold"></div>
-          </div>
-          <ul className="flex space-x-6 text-[#22467d] text-xl font-semibold font-['Poppins']">
-            <li>
-              <a
-                href="/"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Profile
-              </a>
-            </li>
-            <li>
-            <div className="text-blue-900 font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/sejarah"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Sejarah
-              </a>
-            </li>
-            <li>
-            <div className="text-blue-900 font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/kegiatan"
-                className="text-blue-600 font-semibold hover:text-blue-500"
-              >
-                Kegiatan
-              </a>
-            </li>
-            <li>
-            <div className="text-blue-900 font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/artikel"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Artikel
-              </a>
-            </li>
-            <li>
-            <div className="text-blue-900 font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/hubungikami"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Hubungi Kami
-              </a>
-            </li>
-            <li>
-            <div className="text-blue-900 font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/tentang"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Tentang Kami
-              </a>
-            </li>
-          </ul>
-          <div className="relative ">
-            <input
-              type="text"
-              placeholder="Cari..."
-              className=" w-32 h-[35px] p-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 mr-28"
-            />
-            <button className="absolute right-0 top-0 mt-2 mr-28">
-              <svg
-                className="h-5 w-5 text-gray-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-4.35-4.35M18 10a8 8 0 10-8 8 8 8 0 008-8z"
-                />
-              </svg>
-            </button>
-          </div>
-          <a href="/login">
-            <div className="absolute right-16 top-6 w-[84px] h-[35px] bg-white rounded-[10px]">
-              <div className=" absolute top-1 right-5 text-blue-900 text-base font-semibold font-['Poppins']">
-                Login
-              </div>
-            </div>
-          </a>
-        </nav>
-      </header>
+      
+
       <br />
       <br />
       <br />
       <br />
       <br />
 
-      <div className="text-[#3c3c3c] text-[40px] font-bold font-['Poppins'] text-center">
+      <div className="text-[#3c3c3c] text-[40px] font-bold font-['Poppins'] text-center mt-4">
         KEGIATAN
       </div>
+
       <div className="p-4">
         {kegiatanData.map((kegiatan) => (
-          <div className="bg-white shadow-md rounded-lg p-6">
+          <div className="bg-white shadow-md rounded-lg p-6 mb-6">
             <a href={`/kegiatan1/${kegiatan.id}`}>
               <div className="text-[#3c3c3c] text-4xl font-semibold font-['Poppins']">
                 {kegiatan.judul}
@@ -143,12 +50,9 @@ const Kegiatan = () => {
               </div>
             </a>
             <br />
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {kegiatan.fotoUrls.map((url, index) => (
-                <div
-                  key={index}
-                  className="rounded-lg overflow-hidden shadow-md"
-                >
+                <div key={index} className="rounded-lg overflow-hidden shadow-md">
                   <img
                     src={url}
                     alt="kegiatan1"
@@ -163,11 +67,11 @@ const Kegiatan = () => {
 
       <footer>
         <div className="bg-blue-900 text-white p-9">
-          <div className="flex justify-between items-start">
+          <div className="flex flex-col md:flex-row justify-between items-start gap-8">
             <div className="flex items-start">
               <img className="w-[77px] h-16" src="logo.png" alt="logo" />
               <div>
-                <div className="text-white text-[42px] font-bold font-['Poppins'] ">
+                <div className="text-white text-[42px] font-bold font-['Poppins']">
                   CV. SOBAR JAYA
                 </div>
                 <br />
@@ -185,8 +89,8 @@ const Kegiatan = () => {
                 </div>
               </div>
             </div>
-            <div className="flex-grow border-l border-white mx-8"></div>
-            <div className="flex space-x-16">
+            <div className="flex-grow border-t md:border-l border-white mt-8 md:mt-0"></div>
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-16 mt-8 md:mt-0">
               <div>
                 <div className="text-white text-2xl font-bold font-['Poppins']">
                   Company
@@ -221,32 +125,10 @@ const Kegiatan = () => {
                 </div>
               </div>
             </div>
-            <br />
-            <br />
-            <br />
-          </div>
-          <div className="flex items-right justify-end right-50 ">
-            <input
-              type="text"
-              placeholder=" "
-              className="p-2 rounded relative top-[-3rem]"
-            ></input>
-            <button className="bg-blue-700 p-2 rounded relative top-[-3rem] ">
-              Cari
-            </button>
           </div>
           <div className="flex justify-end mt-4 space-x-4">
-            <p>Follow Us</p>
-            <a href="#" className="text-white"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-twitter"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-instagram"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-youtube"></i></a>
-          </div>
-          <br></br>
-    
-          <div className="w-[1192.02px] h-[0px] border border-white"></div>
-          <div className="mt-8 text-center">
-            <p>Copyright 2024 © CV. Sobar Jaya Kota Bandung</p>
+            <p>
+            </p>
           </div>
         </div>
       </footer>
--- frontend/src/component/sejarah.jsx
+++ frontend/src/component/sejarah.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Navigator from "./Navbar";
+import BackToTop from './BackToTop';
 
 
 const Sejarah = ({ id = 2 }) => {
   const [sejarahDta, setSejarahDta] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const fetchSejarah = async () => {
       try {
-
-        const response = await axios.get(`http://localhost:8000/api/showSejarah/4`);
-
+        const response = await axios.get(`http://localhost:8000/api/showSejarah/1`);
         setSejarahDta(response.data);
       } catch (err) {
         setError('Data tidak ditemukan atau terjadi kesalahan');
@@ -33,124 +34,30 @@ const Sejarah = ({ id = 2 }) => {
 
   return (
     <>
-  
-      <header>
-      <nav className="bg-blue-900 shadow-md fixed w-800 top-[3%] left-3 right-3 z-10 flex justify-between items-center p-4 rounded-full h-18">
-          <div className="flex items-center">
-            <img src="logo.png" alt="Logo" className="h-12 mr-2" />
-            <div className="text-white text-2xl font-bold"></div>
-          </div>
-          <ul className="flex space-x-6 text-[#22467d] text-xl font-semibold font-['Poppins']">
-            <li>
-              <a
-                href="/"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Profile
-              </a>
-            </li>
-            <li>
-            <div className="text-white font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/sejarah"
-                className="text-blue-500 font-semibold hover:text-blue-500"
-              >
-                Sejarah
-              </a>
-            </li>
-            <li>
-            <div className="text-white font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/kegiatan"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Kegiatan
-              </a>
-            </li>
-            <li>
-            <div className="text-white font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/artikel"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Artikel
-              </a>
-            </li>
-            <li>
-            <div className="text-white font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/hubungikami"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Hubungi Kami
-              </a>
-            </li>
-            <li>
-            <div className="text-white font-semibold hover:text-blue-500">|</div>
-            </li>
-            <li>
-              <a
-                href="/tentang"
-                className="text-white font-semibold hover:text-blue-500"
-              >
-                Tentang Kami
-              </a>
-            </li>
-          </ul>
-          <div className="relative ">
-            <input
-              type="text"
-              placeholder="Cari..."
-              className=" w-32 h-[35px] p-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 mr-28"
-            />
-            <button className="absolute right-0 top-0 mt-2 mr-28">
-              <svg
-                className="h-5 w-5 text-gray-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-4.35-4.35M18 10a8 8 0 10-8 8 8 8 0 008-8z"
-                />
-              </svg>
-            </button>
-          </div>
-          <a href="/login">
-            <div className="absolute right-16 top-6 w-[84px] h-[35px] bg-white rounded-[10px]">
-              <div className=" absolute top-1 right-5 text-blue-900 text-base font-semibold font-['Poppins']">
-                Login
-              </div>
-            </div>
-          </a>
-        </nav>
-      </header>
+    <Navigator />
+      <BackToTop/>
+    
+
 
       <br />
       <br />
       <br />
       <br />
       <br />
-      
-      <main className="container mx-auto my-5">
+
+      <main className="container mx-auto px-4 my-5">
         <div className="bg-white shadow-md rounded-lg overflow-hidden">
           {sejarahDta ? (
             <>
-              <img src={sejarahDta.foto} alt="Foto Sejarah" className="w-full" />
-              <div className="p-6">
-                <div className="text-[#3c3c3c] text-[40px] font-bold text-center">SEJARAH </div>
-                <div className="w-[1206px] h-[283px] text-[#3c3c3c] text-[32px] font-medium">{sejarahDta.sejarah}</div>
+              <img src={sejarahDta.foto} alt="Foto Sejarah" className="w-full max-h-64 md:max-h-96 object-cover" />
+              <div className="p-4 md:p-6">
+                <div className="text-[#3c3c3c] text-xl md:text-[40px] font-bold text-center">
+                  SEJARAH
+                </div>
+                <div className="text-[#3c3c3c] text-sm md:text-[32px] font-medium leading-relaxed md:leading-[1.5]">
+  {sejarahDta.sejarah}
+</div>
+
               </div>
             </>
           ) : (
@@ -158,94 +65,56 @@ const Sejarah = ({ id = 2 }) => {
           )}
         </div>
       </main>
-      
-      <footer>
-        <div className="bg-blue-900 text-white p-9">
-          <div className="flex justify-between items-start">
-            <div className="flex items-start">
-              <img className="w-[77px] h-16" src="logo.png" alt="logo" />
-              <div>
-                <div className="text-white text-[42px] font-bold font-['Poppins'] ">
-                  CV. SOBAR JAYA
-                </div>
-                <br />
-                <div className="text-white text-xl font-semibold font-['Poppins']">
-                  ALAMAT :JLN. CIBEUNYING PERMAI V NO. 7 CIGADUNG
-                  <br />
-                  BANDUNG 40191 INDONESIA
-                </div>
-                <div className="text-white text-xl font-semibold font-['Poppins']">
-                  PHONE :<br />+ 0123-4567-8910
-                </div>
-                <div className="text-white text-xl font-semibold font-['Poppins']">
-                  EMAIL :<br />
-                  E.MAILKAMI@EMAIL.COM
-                </div>
+
+      <footer className="bg-blue-900 text-white p-6 md:p-9">
+        <div className="flex flex-col md:flex-row justify-between items-start">
+          <div className="flex items-start">
+            <img className="w-12 md:w-[77px] h-10 md:h-16" src="logo.png" alt="logo" />
+            <div className="ml-4">
+              <div className="text-lg md:text-[42px] font-bold">CV. SOBAR JAYA</div>
+              <div className="text-sm md:text-xl font-semibold">
+                ALAMAT: JLN. CIBEUNYING PERMAI V NO. 7 CIGADUNG, BANDUNG 40191 INDONESIA
               </div>
+              <div className="text-sm md:text-xl font-semibold">PHONE: +0123-4567-8910</div>
+              <div className="text-sm md:text-xl font-semibold">EMAIL: E.MAILKAMI@EMAIL.COM</div>
             </div>
-            <div className="flex-grow border-l border-white mx-8"></div>
-            <div className="flex space-x-16">
-              <div>
-                <div className="text-white text-2xl font-bold font-['Poppins']">
-                  Company
-                </div>
-                <br />
-                <div className="text-white text-xl font-normal font-['Poppins']">
-                  Kegiatan
-                </div>
-                <div className="text-white text-xl font-normal font-['Poppins']">
-                  Tentang Kami
-                </div>
-                <div className="text-white text-xl font-normal font-['Poppins']">
-                  Sejarah
-                </div>
-              </div>
-              <div>
-                <div className="text-white text-2xl font-bold font-['Poppins']">
-                  Costumer Service
-                </div>
-                <br />
-                <div className="text-white text-xl font-normal font-['Poppins']">
-                  Hubungi Kami
-                </div>
-              </div>
-              <div>
-                <div className="text-white text-2xl font-bold font-['Poppins']">
-                  Content
-                </div>
-                <br />
-                <div className="text-white text-xl font-normal font-['Poppins']">
-                  Artikel
-                </div>
-              </div>
+          </div>
+          <div className="flex-grow border-t md:border-l border-white mx-4 my-4 md:my-0"></div>
+          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-16">
+            <div>
+              <div className="text-white text-lg md:text-2xl font-bold">Company</div>
+              <div className="text-sm md:text-xl font-normal">Kegiatan</div>
+              <div className="text-sm md:text-xl font-normal">Tentang Kami</div>
+              <div className="text-sm md:text-xl font-normal">Sejarah</div>
             </div>
-            <br />
-            <br />
-            <br />
+            <div>
+              <div className="text-white text-lg md:text-2xl font-bold">Customer Service</div>
+              <div className="text-sm md:text-xl font-normal">Hubungi Kami</div>
+            </div>
+            <div>
+              <div className="text-white text-lg md:text-2xl font-bold">Content</div>
+              <div className="text-sm md:text-xl font-normal">Artikel</div>
+            </div>
           </div>
-          <div className="flex items-right justify-end right-50 ">
-            <input
-              type="text"
-              placeholder=" "
-              className="p-2 rounded relative top-[-3rem]"
-            ></input>
-            <button className="bg-blue-700 p-2 rounded relative top-[-3rem] ">
-              Cari
-            </button>
-          </div>
-          <div className="flex justify-end mt-4 space-x-4">
-            <p>Follow Us</p>
-            <a href="#" className="text-white"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-twitter"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-instagram"></i></a>
-                    <a href="#" className="text-white"><i class="fab fa-youtube"></i></a>
-          </div>
-          <br></br>
-    
-          <div className="w-[1192.02px] h-[0px] border border-white"></div>
-          <div className="mt-8 text-center">
-            <p>Copyright 2024 © CV. Sobar Jaya Kota Bandung</p>
-          </div>
+        </div>
+        <div className="flex justify-center md:justify-end mt-4 space-x-4">
+          <p>Follow Us</p>
+          <a href="#" className="text-white">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="text-white">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="text-white">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#" className="text-white">
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+        <div className="w-full border-t border-white mt-4"></div>
+        <div className="mt-4 text-center text-sm md:text-base">
+          <p>Copyright 2024 © CV. Sobar Jaya Kota Bandung</p>
         </div>
       </footer>
     </>
